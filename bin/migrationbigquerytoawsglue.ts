d7684a01622a4c218d1ce9c25749df05d26a8963
RodrigoRoldan-09 <rodrigoem.roldan@gmail.com>
2025-04-12 10:06:58 -0600

    bin debugging

--- bin/migrationbigquerytoawsglue.ts
+++ bin/migrationbigquerytoawsglue.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { migrationbigquerytoawsglue } from '../lib/migrationbigquerytoawsglue';
+import { BigQueryToGlueStack } from '../lib/migrationbigquerytoawsglue';
 
 
 const app = new cdk.App();
-new migrationbigquerytoawsglue(app, 'migrationbigquerytoawsglue', {
+new BigQueryToGlueStack (app, 'BigQueryToGlueStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
